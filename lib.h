93b4bd4c395a7de35a14c315aa9792b523b21999
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-01 00:04:36 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,4 +2,4 @@
 #ifndef 06-numeri-primi-Andrea-Fiori_lib_h
 #define 06-numeri-primi-Andrea-Fiori_lib_h
 bool scm(int a, int b);
-#endif //06-numeri-primi-Andrea-Fiori
+#endif //06-numeri-primi-Andrea-Fiori_lib_h

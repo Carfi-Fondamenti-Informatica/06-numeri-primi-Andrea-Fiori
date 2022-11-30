f0583ce0da6477b9ad93a27f863ba53e12dcf679
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-11-30 23:51:41 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,6 @@
 
-#ifndef ESERCITAZIONE_6_DEC_H
-#define ESERCITAZIONE_6_DEC_H
+#ifndef 06-numeri-primi-Andrea-Fiori
+#define 06-numeri-primi-Andrea-Fiori
 bool scm(int a, int b);
 void verifica();
 #endif //06-numeri-primi-Andrea-Fiori

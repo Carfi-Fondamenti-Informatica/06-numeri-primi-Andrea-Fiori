cf1344f719dc1a4f2bed88468021f1d7c31be9dd
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-11-30 23:50:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-
+#incliude "lib.h"
 int main(){
+  int a=0;
+  cin>>a;
+  if (scm(a,a)){
+        cout << "numero primo";
+    }else {
+        cout << "numero non primo";
+    }
   return 0;
 }
